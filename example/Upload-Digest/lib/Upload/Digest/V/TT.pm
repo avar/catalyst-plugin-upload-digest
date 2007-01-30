59a61e7c2503dcb0486e3f1341372f293f0b9aa8
AEvar Arnfjord Bjarmason <avar@cpan.org>
2007-01-29 19:42:34 -0800

    initial import of Catalyst-Plugin-Upload-Digest 0.01 from CPAN
    git-cpan-module:   Catalyst-Plugin-Upload-Digest
    git-cpan-version:  0.01
    git-cpan-authorid: AVAR
    git-cpan-file:     authors/id/A/AV/AVAR/Catalyst-Plugin-Upload-Digest-0.01.tar.gz

---
+++ example/Upload-Digest/lib/Upload/Digest/V/TT.pm
@@ -0,0 +1,17 @@
+package # Hide from PAUSE
+Upload::Digest::V::TT;
+
+use strict;
+use base qw< Catalyst::View::TT >;
+
+=head1 NAME
+
+Upload::Digest::V::TT - TT view for L<Upload::Digest>
+
+=head1 DESCRIPTION
+
+This is just a wrapper around L<Catalyst::View::TT>
+
+=cut
+
+1;
